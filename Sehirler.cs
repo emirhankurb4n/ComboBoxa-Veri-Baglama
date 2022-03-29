8185d6d47873982537e98b2d92255a3a9be067b7
Emirhan Kurban <100554100+emirhankurb4n@users.noreply.github.com>
2022-03-29 09:38:11 +0300

    Kodlar

---
+++ Sehirler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication27
+{
+    class Sehirler
+    {
+        public string Plaka { get; set; }
+        public string SehirAdi { get; set; }
+    }
+}
+
